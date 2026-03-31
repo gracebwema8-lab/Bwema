dde2c394c108e7399823577dfe05e0e9b54dd6ec
gracebwema8-lab <gracebwema8@gmail.com>
2026-03-31 17:48:17 +0200

    push_30_03_26

---
+++ css/# JS sript.js
@@ -0,0 +1,12 @@
+document.write("HELLO WORLD");
+alert("attention nous sommes programmeurs");
+console.log("salut congolais");
+*/
+const annee_de_naissance = 2005;
+const annee_actuelle = 2026;
+let age= annee_actuelle-annee_de_naissance 
+console.log(age)
+
+if (age>=18){
+console.log (majeur); 
+}else  
---
+++ css/opration.js
@@ -0,0 +1,56 @@
+document.write("HELLO WORLD");
+alert("attention nous sommes programmeurs");
+console.log("salut congolais");
+*/
+const PA = 15000;
+const PV = 10000;
+let B= PA-PV
+console.log(Benefice)
+
+if (B<0){
+console.log ("gain"); 
+}else{
+    console.log ("perte") 
+}
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Programmation web1</title>
+    <link rel="stylesheet" href="C:\Users\Lydie\Desktop\Cours HTML\style.css" />
+  </head>
+  <body>
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <a href="C:\Users\Lydie\Desktop\Cours HTML\Acceuil.html">
+              Acceuil
+            </a>
+          </li>
+
+          <li>
+            <a href="C:\Users\Lydie\Desktop\Cours HTML\contact.html">Contact</a>
+          </li>
+          <li>
+            <a href="C:\Users\Lydie\Desktop\Cours HTML\inscription.html"
+              >Inscription</a
+            >
+          </li>
+          <li>
+            <a href="C:\Users\Lydie\Desktop\Cours HTML\connexion.html"
+              >Connexion</a
+            >
+          </li>
+        </ul>
+      </nav>
+    </header>
+
+    <div class="main">
+      <h1>BIENVENUE</h1>
+    </div>
+
+    <footer></footer>
+  </body>
+</html>
